85c2d83b4896fde7a15d041e40e96b13a505470f
anaelisaq <99890284+anaelisaq@users.noreply.github.com>
2022-02-17 11:06:51 -0300

    Add files via upload

---
+++ First.java
@@ -0,0 +1,11 @@
+package com.mycompany.first;
+
+/*Ana Elisa  */
+public class First {
+    public static void main(String[] args) {
+        int x,y;
+        x=2;
+        y=x*2;
+        System.out.print (y);
+    }
+}

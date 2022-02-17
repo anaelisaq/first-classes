6a5dc2313105a26a7f4f4cb3f0766301840c9ee3
Ana Elisa Alexandre <99890284+anaelisaq@users.noreply.github.com>
2022-02-17 11:47:33 -0300

    Add files via upload

---
+++ Second.java
@@ -0,0 +1,11 @@
+package com.mycompany.first;
+import java.util.Scanner;
+/*Ana Elisa */
+public class Second {
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        String nome=in.nextLine();
+        System.out.println(nome);
+        System.out.print("\n"+nome);
+    }
+}
